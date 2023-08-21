6861e655a7f5badf3e850f884c3c726da1085e04
Taka31 <cyrilmenard78@gmail.com>
2023-08-21 18:35:53 +0200

    TP Part 5.3

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    production:false,
+    firebase:{
+        apiKey: "AIzaSyA5rnz4nS6RDVgkgWxG3U_0SRL8YLwQxGQ",
+        authDomain: "buyandsellv3.firebaseapp.com",
+        projectId: "buyandsellv3",
+        storageBucket: "buyandsellv3.appspot.com",
+        messagingSenderId: "504518094941",
+        appId: "1:504518094941:web:89d1c955876ce701e61db4"
+      },
+};
